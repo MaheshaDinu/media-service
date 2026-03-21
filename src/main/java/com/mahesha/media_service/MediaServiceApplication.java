b9adf9f3e2439ea995e63955f7336189f5a97ef3
MaheshaDinu98 <maheeshadinushan@gmail.com>
2026-03-21 16:55:52 +0530

    Setup media-service with Google Cloud Storage (excluding secrets)

--- src/main/java/com/mahesha/media_service/MediaServiceApplication.java
+++ src/main/java/com/mahesha/media_service/MediaServiceApplication.java
@@ -1,13 +1,29 @@
 package com.mahesha.media_service;
 
+import java.io.IOException;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.io.ClassPathResource;
+
+import com.google.auth.oauth2.ServiceAccountCredentials;
+import com.google.cloud.storage.Storage;
+import com.google.cloud.storage.StorageOptions;
 
 @SpringBootApplication
 public class MediaServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MediaServiceApplication.class, args);
+	}
+
+	@Bean
+	public Storage storage(@Value("${gcp.project-id}") String projectId, @Value("${gcp.credentials.location}") ClassPathResource credentialsResource) throws IOException {
+
+		ServiceAccountCredentials credentials = ServiceAccountCredentials.fromStream(credentialsResource.getInputStream());
+		return StorageOptions.newBuilder().setProjectId(projectId).setCredentials(credentials).build().getService();
 	}
 
 }
