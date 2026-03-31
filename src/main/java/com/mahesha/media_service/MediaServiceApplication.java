e56df64180e1957ed5e8c630eb1f3ad20cb1ff01
MaheshaDinu98 <maheeshadinushan@gmail.com>
2026-03-31 09:59:11 +0530

    changed code access bucket through gcp

--- src/main/java/com/mahesha/media_service/MediaServiceApplication.java
+++ src/main/java/com/mahesha/media_service/MediaServiceApplication.java
@@ -2,13 +2,10 @@ package com.mahesha.media_service;
 
 import java.io.IOException;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.core.io.ClassPathResource;
 
-import com.google.auth.oauth2.ServiceAccountCredentials;
 import com.google.cloud.storage.Storage;
 import com.google.cloud.storage.StorageOptions;
 
@@ -20,10 +17,11 @@ public class MediaServiceApplication {
 	}
 
 	@Bean
-	public Storage storage(@Value("${gcp.project-id}") String projectId, @Value("${gcp.credentials.location}") ClassPathResource credentialsResource) throws IOException {
+	public Storage storage() throws IOException {
 
-		ServiceAccountCredentials credentials = ServiceAccountCredentials.fromStream(credentialsResource.getInputStream());
-		return StorageOptions.newBuilder().setProjectId(projectId).setCredentials(credentials).build().getService();
+		// ServiceAccountCredentials credentials = ServiceAccountCredentials.fromStream(credentialsResource.getInputStream());
+		// return StorageOptions.newBuilder().setProjectId(projectId).setCredentials(credentials).build().getService();
+		return StorageOptions.getDefaultInstance().getService();
 	}
 
 }
